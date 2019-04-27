be41cf878a75033e3b960d5b90d812ce93442e08
Jason <jason@JasondeMacBook-Pro.local>
2019-04-28 07:49:45 +0800

    13.0

---
+++ src/ch39/unsafe_programming/unsafe_test.go
@@ -0,0 +1,13 @@
+package unsafe_programming
+
+import (
+	"testing"
+	"unsafe"
+)
+
+func TestUnsafe(t *testing.T) {
+	i := 10
+	f := *(*float64)(unsafe.Pointer(&i))
+	t.Log(unsafe.Pointer(&i))
+	t.Log(f)
+}
